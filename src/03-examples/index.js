ab42087eff447764805a7dcd617a2c4a147e547d
Israelf25 <desarrollo3@perzona.mx>
2025-03-25 21:09:32 -0600

    useLayoutEffect

---
+++ src/03-examples/index.js
@@ -0,0 +1,3 @@
+export * from './LoadingMessage'
+export * from './MultipleCustomsHooks'
+export * from './PokemonCard'
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
+//import { FocusScreen } from './04-useRef/FocusScreen'
 //import { MultipleCustomsHooks } from './03-examples/MultipleCustomsHooks'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -13,5 +14,5 @@ import { FocusScreen } from './04-useRef/FocusScreen'
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   // </StrictMode>
-    <FocusScreen/>
+    <Layout/>
 )
