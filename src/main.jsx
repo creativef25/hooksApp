321a66aaaa78ef55d8334aa7bc63d4852fc069a7
Israelf25 <desarrollo3@perzona.mx>
2025-04-01 22:12:24 -0600

    useContest a medias

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
 
 //import './08-useReducer/intro-reducer'
 
 import './index.css'
-import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
+//import { TodoApp } from './08-useReducer/TodoApp'
 //import { CallbackHook } from './06-memos/CallbackHook'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { Memorize } from './06-memos/Memorize'
@@ -22,5 +24,7 @@ import { TodoApp } from './08-useReducer/TodoApp'
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   // </StrictMode>
-  <TodoApp/>
+  <BrowserRouter>
+    <MainApp/>
+  </BrowserRouter>
 )
